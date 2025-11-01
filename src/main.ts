ceee2da655375b3b3c71dab8cafb8a3faf8c59c2
Gustavo <Gustavotubazo@gmail.com>
2025-11-01 08:05:47 -0600

    Test: log del entorno actual

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/components/app.component';
+import { environment } from './environments/environment'; // ✅ IMPORTA ENVIRONMENT
+
+console.log('🌍 Entorno actual:', environment); // ✅ CONFIRMAR ENTORNO
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
